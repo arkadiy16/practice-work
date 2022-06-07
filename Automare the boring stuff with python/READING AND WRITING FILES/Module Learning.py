9f96988b569857b5ea9012b4557d7df298b15278
arkadiy16 <36086739+arkadiy16@users.noreply.github.com>
2022-06-07 21:08:05 +0300

    relative and absolute paths

--- Automare the boring stuff with python/READING AND WRITING FILES/Module Learning.py
+++ Automare the boring stuff with python/READING AND WRITING FILES/Module Learning.py
@@ -39,3 +39,21 @@ print(d) # C:\Users\Docs\NewFolder
 # Introdaction with os.makedirs() and Path().mkdir().
 os.makedirs('C:\\Users\\Desktop\\New\\Foo\\Bar') # Create new folders: \New\Foo\Bar
 Path('C:\\Users\\1\\Desktop\\FBar').mkdir() # Create only 1 folder: \FBar
+
+
+# Introduction with absolute and relative paths.
+print(Path.cwd()) # C:\Users\1\Desktop
+print(Path.cwd().is_absolute()) # True
+print(Path('foo/bar').is_absolute()) # False
+print(os.path.isabs(Path.cwd())) # Same as line before previous but with os module.
+
+
+relative_path = Path('Foo/Bar') 
+print(Path.cwd() / relative_path) # Making absolute path from relative path with / operator and Path.cwd() function
+print(os.path.abspath(f'.\\{relative_path}')) # Same as past but with os.path.abspath() function.
+
+print(os.path.abspath('.')) # Absolute path to cwd.
+print(os.path.abspath('..')) # Absolute path to parent folder cwd.
+print(os.path.abspath('\\')) # Local disk of cwd.
+
+print(os.path.relpath('C:\\Users\\1\\Desktop', '\\Users')) # Turning absolute path to relative.
