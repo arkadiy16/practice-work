2ca63b8c575f8299c4a1d8d0cc748237b326645b
arkadiy16 <36086739+arkadiy16@users.noreply.github.com>
2022-06-23 21:06:33 +0300

    Update Module Learning.py

--- Automare the boring stuff with python/DEBUGGING/Module Learning.py
+++ Automare the boring stuff with python/DEBUGGING/Module Learning.py
@@ -1,6 +1,10 @@
 import logging
 
+
+# level=logging.LEVEL - will show only LEVEL and higher level.
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s -  %(message)s')
+
+# Logging levels.
 logging.debug('Some debugging details.')
 # 2019-05-18 19:04:26,901 - DEBUG - Some debugging details.
 logging.info('The logging module is working.')
@@ -11,3 +15,18 @@ logging.error('An error has occurred.')
 # 2019-05-18 19:05:07,737 - ERROR - An error has occurred.
 logging.critical('The program is unable to recover!')
 # 2019-05-18 19:05:45,794 - CRITICAL - The program is unable to recover!
+
+# Disabling logging.
+# logging.LEVEL - will disable LEVEL and lower lewer.
+logging.disable(logging.WARNING) 
+logging.debug('Some debugging details.')
+logging.info('The logging module is working.')
+logging.warning('An error message is about to be logged.')
+logging.error('An error has occurred.')
+# 2019-05-18 19:05:07,737 - ERROR - An error has occurred.
+logging.critical('The program is unable to recover!')
+# 2019-05-18 19:05:45,794 - CRITICAL - The program is unable to recover!
+
+# Logging to file.
+logging.basicConfig(filename='logfile.txt', level=logging.DEBUG, 
+                    format=' %(asctime)s - %(levelname)s -  %(message)s')
