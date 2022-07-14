1cfc89045845c8fe687c048afe9424071e489377
arkadiy16 <36086739+arkadiy16@users.noreply.github.com>
2022-07-15 00:54:24 +0300

    Update Learning Module.py

--- Automare the boring stuff with python/WEB SCRAPING/Learning Module.py
+++ Automare the boring stuff with python/WEB SCRAPING/Learning Module.py
@@ -12,3 +12,21 @@ else:
     address = pyperclip.paste()
 
 webbrowser.open('https://www.google.com/maps/place/' + address)
+
+
+import bs4
+exampleFile = open('example.html')
+exampleSoup = bs4.BeautifulSoup(exampleFile.read(), 'html.parser')
+elems = exampleSoup.select('#author')
+type(elems) # elems is a list of Tag objects.
+# <class 'list'>
+len(elems)
+# 1
+type(elems[0])
+# <class 'bs4.element.Tag'>
+str(elems[0]) # The Tag object as a string.
+# '<span id="author">Al Sweigart</span>'
+elems[0].getText()
+# 'Al Sweigart'
+elems[0].attrs
+# {'id': 'author'}
