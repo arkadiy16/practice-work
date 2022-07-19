f346fd228da5bc328afb9eff71fe0108ec2524f7
arkadiy16 <36086739+arkadiy16@users.noreply.github.com>
2022-07-19 19:38:33 +0300

    Update Learning Module.py

--- Automare the boring stuff with python/WEB SCRAPING/Learning Module.py
+++ Automare the boring stuff with python/WEB SCRAPING/Learning Module.py
@@ -30,3 +30,18 @@ elems[0].getText()
 # 'Al Sweigart'
 elems[0].attrs
 # {'id': 'author'}
+
+soup.select('div')
+# All elements named <div>
+soup.select('#author')
+# The element with an id attribute of author
+soup.select('.notice')
+# All elements that use a CSS class attribute named notice
+soup.select('div span')
+# All elements named <span> that are within an element named <div>
+soup.select('div > span')
+# All elements named <span> that are directly within an element named <div>, with no other element in between
+soup.select('input[name]')
+# All elements named <input> that have a name attribute with any value
+soup.select('input[type="button"]')
+# All elements named <input> that have an attribute named type with value button
