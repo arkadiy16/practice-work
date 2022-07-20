dd8d80a4a1ab086bdd82819c98e5e963ad137ce8
arkadiy16 <arkadiy160702@gmail.com>
2022-07-20 09:55:05 +0300

    third practic

--- Automare the boring stuff with python/WEB SCRAPING/com_rating_joyreactor.py
+++ Automare the boring stuff with python/WEB SCRAPING/com_rating_joyreactor.py
@@ -13,29 +13,32 @@ def main(name):
     pages = int(soup.select_one('.pagination_expanded').select('a')[-1].get_text())
     logging.warning(pages)
     # Loop for every page.
-    for page_num in range(2, pages + 1):
-        linkElems = soup.select('.txt')
-        # Loop for every comment in page.
-        for i in range(len(linkElems)):
-            rat_url = linkElems[i].select_one('.comment_link').get('href').split('#')
-            comment = linkElems[i].select_one('div').get_text()
-            logging.info(rat_url)
-            rat_req = requests.get('https://joyreactor.cc' + rat_url[0])
-            rat_soup = bs4.BeautifulSoup(rat_req.text, 'html.parser')
-            com = rat_soup.select_one(f'#{rat_url[1]}')
-            logging.info(com)
-            # Checking for comment exist.
-            try:
-                com_rat = com.select_one('.comment_rating').get_text()
-            except AttributeError:
-                com_rat = 0
-            # Write comment info in list.
-            data.append([rat_url[0], rat_url[1], comment, float(com_rat)])
-        logging.warning(page_num)
-        # Change page.
-        url = 'https://joyreactor.cc/user/%D0%A7%D1%83%D0%B4%D0%BE%D0%9F%D1%91%D1%81/comments' + f'/{page_num}'
-        res = requests.get(url)
-        soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    for page_num in range(2, 120):
+        try:
+            linkElems = soup.select('.txt')
+            # Loop for every comment in page.
+            for i in range(len(linkElems)):
+                rat_url = linkElems[i].select_one('.comment_link').get('href').split('#')
+                comment = linkElems[i].select_one('div').get_text()
+                logging.info(rat_url)
+                rat_req = requests.get('https://joyreactor.cc' + rat_url[0])
+                rat_soup = bs4.BeautifulSoup(rat_req.text, 'html.parser')
+                com = rat_soup.select_one(f'#{rat_url[1]}')
+                logging.info(com)
+                # Checking for comment exist.
+                try:
+                    com_rat = com.select_one('.comment_rating').get_text()
+                except AttributeError:
+                    com_rat = 0
+                # Write comment info in list.
+                data.append([rat_url[0], rat_url[1], comment, float(com_rat)])
+            logging.warning(page_num)
+            # Change page.
+            url = 'https://joyreactor.cc/user/%D0%A7%D1%83%D0%B4%D0%BE%D0%9F%D1%91%D1%81/comments' + f'/{page_num}'
+            res = requests.get(url)
+            soup = bs4.BeautifulSoup(res.text, 'html.parser')
+        except:
+            continue
 
     data.sort(key=lambda c: c[3], reverse=True)
     # Write comment info in file.
