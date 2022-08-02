f637ddc609dc8f211e5d3ceec1a6c5148636afa5
arkadiy16 <arkadiy160702@gmail.com>
2022-08-02 14:35:30 +0300

    new project

---
+++ The Big Book of Small Python Projects/project3_bitmap.py
@@ -0,0 +1,45 @@
+bitmap = """
+....................................................................
+   **************   *  *** **  *      ******************************
+  ********************* ** ** *  * ****************************** *
+ **      *****************       ******************************
+          *************          **  * **** ** ************** *
+           *********            *******   **************** * *
+            ********           ***************************  *
+   *        * **** ***         *************** ******  ** *
+               ****  *         ***************   *** ***  *
+                 ******         *************    **   **  *
+                 ********        *************    *  ** ***
+                   ********         ********          * *** ****
+                   *********         ******  *        **** ** * **
+                   *********         ****** * *           *** *   *
+                     ******          ***** **             *****   *
+                     *****            **** *            ********
+                    *****             ****              *********
+                    ****              **                 *******   *
+                    ***                                       *    *
+                    **     *                    *
+....................................................................
+"""
+
+
+# function take a string and replaced not empty characters by characters in string.
+def bitmap_replacer(message, btmap):
+    btmap_l = []
+    # Loop over each line in the bitmap:
+    for _, string in enumerate(btmap.split('\n')):
+        for i, elem in enumerate(string):
+            if elem != ' ':
+                string = string.replace(elem, message[i % len(message)], 1)
+        btmap_l.append(string)
+    return btmap_l
+
+
+def main():
+    print('Enter the message to display with the bitmap.')
+    # message = input('> ')
+    message = 'Hello!'
+    print(*bitmap_replacer(message, bitmap), sep='\n')
+
+
+main()
