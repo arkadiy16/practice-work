26ac0099d2bfc397808cdcf794985760372fbfe1
arkadiy16 <arkadiy160702@gmail.com>
2022-08-03 23:18:09 +0300

    Done

--- The Big Book of Small Python Projects/project4_blackjeck.py
+++ The Big Book of Small Python Projects/project4_blackjeck.py
@@ -11,7 +11,8 @@ def card_print(cards):
     else:
         player = 'AI'
     cards = cards[1:]
-    print(f'{player}\'s hand: {point_count(cards)}')
+    points = point_count(cards)
+    print(f'{player}\'s hand: {points}')
     # Printing cards with ASCII.
     for i in range(4):
         if i == 0:
@@ -40,6 +41,7 @@ def card_print(cards):
                 print(f'|_{rank}|', end=' ')
             print()
             print()
+    return points
 
 
 suits = '♥♠♦♣'
@@ -77,9 +79,13 @@ def point_count(hand):
             ace_count -= 1
     return points
 
+
 # AI.
-def ai():
-    pass
+def ai(a_hand, deck):
+    while point_count(a_hand[1:]) < 17:
+        print(1)
+        a_hand.append(deck.pop())
+    return a_hand, deck
 
 
 # Input validation for bet.
@@ -89,13 +95,95 @@ def bet_validation(num, money=5000):
     try:
         bet = int(num)
     except:
-        raise Exception('Bet should be digit.')
+        raise Exception('Bet should be integer digit.')
     if bet > money:
         raise Exception(f'Bet can\'t be more than you have.(<{money})')
     elif bet < 1:
         raise Exception('Bet should be at least 1.')
     print(f'Bet: {bet}', end='')
     return bet
+
+
+# Player turn.
+def p_turn(p_hand, deck):
+    while True and point_count(p_hand[1:]) < 21:
+        choice =  pyip.inputChoice(['h', 's', 'd'])
+        if choice == 's':
+            break
+        elif choice == 'h':
+            hit_card = deck.pop()
+            print(f'You drew a {hit_card[1]} of {hit_card[0]}.')
+            p_hand.append(hit_card)
+        else:
+            p_hand.append(deck.pop())
+            break
+    return p_hand, deck
+
+
+# BJ game.
+def bj():
+    money = 5000
+    deck = deck_create()
+    while True:
+        while True:
+            print(f'You have: ${money}')
+            print(f'How much do you bet? (1-{money}, or QUIT)')
+            bet =  pyip.inputCustom(bet_validation)
+            if not bet:
+                break
+            p_hand = ['p', deck.pop(), deck.pop()]
+            p_points = card_print(p_hand)
+            face_down_card = deck.pop()
+            a_hand = ['a', face_down_card, deck.pop()]
+            a_points = point_count(a_hand[1:])
+            # Checking if any hand have bj.
+            if p_points == 21 and a_points == 21:
+                print(f'Draw!')
+                break
+            elif p_points == 21:
+                print(f'Congratulations, you got Blackjeck!!!\nYou won: ${bet * 3 / 2}.')
+                money += bet * 3 / 2
+                break
+            elif a_points == 21:
+                print(f'Dealer got blackjeck!!!')
+                card_print(a_hand)
+                break
+            face_down_card[2] = 'down'
+            print()
+            card_print(a_hand)
+            face_down_card[2] = 'up'
+            print('(H)it, (S)tand, (D)ouble down')
+            p_hand, deck = p_turn(p_hand, deck)
+            if point_count(p_hand[1:]) > 21:
+                money -= bet
+                card_print(p_hand)
+                card_print(a_hand)
+                print('You ower!')
+                break
+            a_hand, deck = ai(a_hand, deck)
+            p_points = card_print(p_hand)
+            a_points = card_print(a_hand)
+            if p_points > a_points or a_points > 21:
+                print('Congratulations! You won: ${bet}.')
+                money += bet
+            else:
+                print('Dealer won.')
+                money -= bet
+
+        choise = pyip.inputYesNo('Do you want to play again? (y/n)')
+        if choise[0] == 'y':
+            if money > 1:
+                choise = pyip.inputYesNo('Do you want to continue with same money you have? (y/n)')
+            if choise[0] == 'n':
+                money = 5000
+                if len(deck) < 50:
+                    deck = deck_create()
+            if len(deck) < 50:
+                deck = deck_create()
+
+            continue
+        else:
+            break
 
 
 def main():
@@ -111,19 +199,8 @@ def main():
       In case of a tie, the bet is returned to the player.
       The dealer stops hitting at 17.
       ''')
-    money = 5000
-    while True:
-        print(f'Money: {money}')
-        print(f'How much do you bet? (1-{money}, or QUIT)')
-        bet = pyip.inputCustom(bet_validation)
-        if not bet:
-            print('Thank you for play.')
-            break
+    bj()
+    print('Thank you for play.')
 
-        deck = deck_create()
-        p_hand = ['p', deck.pop(), deck.pop()]
-        card_print(p_hand)
-        face_down_card = deck.pop()
-        face_down_card[2] = 'down'
-        a_hand = ['a', face_down_card, deck.pop()]
-        card_print(a_hand)
+
+main()
