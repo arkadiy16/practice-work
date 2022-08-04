f9b9011cb8bc659970be029d0fe25ffbe0af9128
arkadiy16 <arkadiy160702@gmail.com>
2022-08-04 15:08:22 +0300

    Now counter work right, logo can start move in "any" direction

--- The Big Book of Small Python Projects/project5_bouncingdvd.py
+++ The Big Book of Small Python Projects/project5_bouncingdvd.py
@@ -17,10 +17,10 @@ def logo_move(screen, x, y, clock):
     pygame.display.update()
 
     # Move text.
-    speed_x = 1
-    speed_y = 1
+    speed_x = random.choice((1, -1))
+    speed_y = random.choice((1, -1))
     while True:
-        clock.tick(60)
+        clock.tick(500)
         screen.fill((255, 255, 255))  # Clear screen.
         counter_t = counter_f.render(f'Corner bounces: {counter}', True, (0, 0, 0))
         screen.blit(counter_t, (25, 0))
@@ -28,14 +28,18 @@ def logo_move(screen, x, y, clock):
         text_x += speed_x
         text_y += speed_y
 
-        # Logo hit the edge it will change speed_x/y to opposite and change logo collor to random.
-        if text_x + 45 == x or text_x == 0:
+        # Logo hit the edge it will change speed_x/y to opposite and change logo color to random.
+        if (text_x + 45 == x or text_x == 5) and (text_y + 20 == y or text_y == 20):
             speed_x = -speed_x
+            speed_y = - speed_y
             counter += 1
+            rgb = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+        if text_x + 45 == x or text_x == 5:
+            speed_x = -speed_x
             rgb = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         if text_y + 20 == y or text_y == 20:
             speed_y = - speed_y
-            counter += 1
             rgb = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         screen.blit(img, (text_x, text_y))
         pygame.display.update()
@@ -45,7 +49,7 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     # Create display where logo will travel.
-    x = 500
+    x = 600
     y = 500
     screen = pygame.display.set_mode((x, y))
     pygame.display.flip()
