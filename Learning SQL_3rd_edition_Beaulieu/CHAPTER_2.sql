a89705bcb333af2c4c7714581aaa06c53c68e1c9
arkadiy16 <36086739+arkadiy16@users.noreply.github.com>
2022-09-07 22:39:52 +0300

    Ex. 3-1

---
+++ Learning SQL_3rd_edition_Beaulieu/CHAPTER_2.sql
@@ -0,0 +1,8 @@
+# Exercise 3-1
+# Retrive the actor ID, first name, last name for all actors. 
+# Sort by last name and then by first name.
+SELECT last_name, first_name, actor_id
+  FROM actor
+  SORT BY last_name, first_name;
+  
+  
