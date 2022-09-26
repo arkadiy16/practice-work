a6b1290c1b7e115d58204b1791be062055674c29
arkadiy16 <36086739+arkadiy16@users.noreply.github.com>
2022-09-27 02:48:19 +0300

    Create CHAPTER_7.sql

---
+++ Learning SQL_3rd_edition_Beaulieu/CHAPTER_7.sql
@@ -0,0 +1,10 @@
+# Exercise 7-1
+# Write a query that returns the 17th through 25th characters of the string 'Please
+# find the substring in this string'.
+mysql> SELECT SUBSTRING('Please find the substring in this string', 17, 9);
+# +--------------------------------------------------------------+
+# | SUBSTRING('Please find the substring in this string', 17, 9) |
+# +--------------------------------------------------------------+
+# | substring                                                    |
+# +--------------------------------------------------------------+
+# 1 row in set (0.00 sec)
