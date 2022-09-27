d374537d491932fa7aff919f53a2f88cee2fb255
arkadiy16 <36086739+arkadiy16@users.noreply.github.com>
2022-09-28 02:49:10 +0300

    Update CHAPTER_7.sql

--- Learning SQL_3rd_edition_Beaulieu/CHAPTER_7.sql
+++ Learning SQL_3rd_edition_Beaulieu/CHAPTER_7.sql
@@ -8,3 +8,32 @@ mysql> SELECT SUBSTRING('Please find the substring in this string', 17, 9);
 # | substring                                                    |
 # +--------------------------------------------------------------+
 # 1 row in set (0.00 sec)
+
+# Exercise 7-2
+# Write a query that returns the absolute value and sign (−1, 0, or 1) of the number
+# −25.76823. Also return the number rounded to the nearest hundredth.
+mysql> SELECT ABS(-25.76823) AS Absulute, SIGN(-25.76823), ROUND(-25.76823, 2);
+# +----------+-----------------+---------------------+
+# | Absulute | SIGN(-25.76823) | ROUND(-25.76823, 2) |
+# +----------+-----------------+---------------------+
+# | 25.76823 |              -1 |              -25.77 |
+# +----------+-----------------+---------------------+
+# 1 row in set (0.00 sec)
+
+# Exercise 7-3
+# Write a query to return just the month portion of the current date.
+mysql> SELECT EXTRACT(MONTH FROM CURRENT_DATE()) AS current_month;
+# +---------------+
+# | current_month |
+# +---------------+
+# |             9 |
+# +---------------+
+# 1 row in set (0.00 sec)
+
+mysql> SELECT MONTHNAME(CURRENT_DATE()) AS current_month;
+# +---------------+
+# | current_month |
+# +---------------+
+# |September     |
+# +---------------+
+# 1 row in set (0.00 sec)
