f362c2bbc7a1886a5b28026e5c3112e1046c3e83
arkadiy16 <arkadiy160702@gmail.com>
2022-11-04 04:14:16 +0300

    appending new function for rounding results. ronding conf limits.

--- PLMC/calculation.py
+++ PLMC/calculation.py
@@ -1,11 +1,11 @@
 # Module for power line transmission calculation.
 
-print(1 + complex(0, 1))
+import tkinter
+
 
 # We know: U(phase)=const; I2; Z12; b12.
 # Need to find: U1; I12; power losses s12_l; I1.
-
-def main(u2, i2, z12, b12):
+def calculate(u2, i2, z12, b12):
     # Calculate capacity current in the end of the line.
     ic12_e = u2 * complex(0, b12) / 2
     # Current in line 12.
@@ -16,5 +16,9 @@ def main(u2, i2, z12, b12):
     ic12_s = u1 * complex(0, b12) / 2
     # Power losses in line (3 phases).
     s12_l = 3 * i12 ** 2 * z12
+    return ic12_e, i12, u1, ic12_s, s12_l
+
+def vect_diagr():
     pass
 
+print(calculate(5890, complex(25, 11), complex(6.23, 2.93), 1))
