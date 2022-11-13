aa13c32bead374bf3c607078029eeb8eb407d992
arkadiy16 <arkadiy160702@gmail.com>
2022-11-14 02:27:49 +0300

    update ts

--- practice_programs/scetch_transformator/transformator_scetch.py
+++ practice_programs/scetch_transformator/transformator_scetch.py
@@ -3,17 +3,34 @@ import turtle
 s = turtle.getscreen()
 t = turtle.Turtle()
 
-t.penup()
-t.setpos(-400, 300)
-t.pendown()
 
-def coil():
+def coil(c_l, a1, a12, a2, a_, l):
+    c_w = 2 * (a1 + a12 + a2)
+    for forw in 2 * (c_w, c_l):
+        t.fd(forw)
+        t.rt(90)
+    t.penup()
+    tp = t.pos()
+    t.goto(tp[0] + a1 + a12 + a2 - a_, tp[1] - (c_l - l) / 2)
+    t.pendown()
+    for forw in 2 * (a1 + a12 + a2, l):
+        t.fd(forw)
+        t.rt(90)
+    t.fd(a2)
+    t.rt(90)
+    t.fd(l)
+    t.rt(-90)
+    t.fd(a12)
+    t.rt(-90)
+    t.fd(l)
     pass
+
 
 def outer_rect(lenght, width):
     for forw in 2 * (lenght, width):
         t.fd(forw)
         t.rt(90)
+
 
 def main():
     d1 = 98
@@ -25,9 +42,26 @@ def main():
     hc = 320
     hi = 85
     c = 213
+
+    t.penup()
+    t.goto(-c - (hi - d / 2), hc / 2 + hi)
+    t.pendown()
+
     outer_rect(2 * c + 2 * (hi - d / 2), hc + 2 * hi)
+
+    t.penup()
+    tp = t.pos()
+    t.goto(tp[0] + hi / 2 + d / 2, tp[1] - hi)
+    t.pendown()
+
+    coil(hc, a1, a12, a2, (d1 - d) / 2, l)
+
+    t.penup()
+    t.goto(c - d / 2, - hc / 2)
+    t.pendown()
+    t.rt(90)
+    coil(-hc, -a1, -a12, -a2, -(d1 - d) / 2, -l)
+
     input()
-
-
 
 main()
